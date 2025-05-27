e61b9eea75f967fc0c1d96cf3e515a1531c6547e
SOFIA C <sofia.calvo456@gmail.com>
2025-05-27 13:01:00 +0200

    COMIT main de prueva sofias

---
+++ src/document2.h
@@ -0,0 +1,22 @@
+#ifndef DOCUMENT2_H
+#define DOCUMENT2_H
+
+typedef struct Link {
+    int destination_id;
+    char* text;
+    struct Link* next;
+} Link;
+
+typedef struct Document {
+    int id;
+    char* title;
+    char* body;
+    Link* links;
+    float relevance_score;
+    struct Document* next;
+} Document;
+
+// Carga un documento desde un archivo en disco
+Document* documentDeserialize(char* path);
+
+#endif // DOCUMENT_H
